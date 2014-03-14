482bfa17ddd27b1dac233e064a68b227b032a1dc
Nicole Paz <nicole@nic.gateway.2wire.net>
2014-03-14 15:35:47 -0700

    initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Secure Stories
+
+Are you paranoid and need to have a password on all of your writings? This is for you! You don't even have to make an account. Just save the link given when you create a new document, and you have your own notepad to work with. 
+
+## License
+
+MIT
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SecureStories::Application.config.secret_key_base = 'd8bb67e8169896bc3d43849dda37264171fb40fdaf39ac982ec242f741d48bb3f01705ed0d2554a40af3154138b07c31c474deb54bd7d04b25796270f3881e5a'
