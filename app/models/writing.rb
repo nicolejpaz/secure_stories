612f4a23ac9d4f0cd768dfce9791b54ce0db782d
Nicole Paz <nicole@nic.gateway.2wire.net>
2014-03-14 15:59:44 -0700

    Generate story_id.

--- app/models/writing.rb
+++ app/models/writing.rb
@@ -1,9 +1,20 @@
 class Writing
+  require 'digest/md5'
   include Rails.application.routes.url_helpers
   include Mongoid::Document
   include Mongoid::Timestamps
+  after_initialize :generate_story_id
   field :story_id, type: String
   field :name, type: String, default: "Unnamed Story"
   field :text, type: String
-  validates_presence_of :story_id
+
+  private
+
+  def generate_story_id
+    all_letters_and_numbers = [*('a'..'z'), *('0'..'9')]
+    sample_of_all_letters_and_numbers = all_letters_and_numbers.sample(10)
+    joined_sample = sample_of_all_letters_and_numbers.join('')
+    sample_hash = Digest::MD5.hexdigest(joined_sample)
+    return sample_hash.slice(0..20)
+  end
 end
