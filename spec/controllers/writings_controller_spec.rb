33056a7bf73b5358a5a7ee963ab38b27c555be9c
Nicole Paz <nicole@nic.gateway.2wire.net>
2014-03-14 16:39:15 -0700

    Add test for writings#show.

--- spec/controllers/writings_controller_spec.rb
+++ spec/controllers/writings_controller_spec.rb
@@ -20,4 +20,16 @@ describe WritingsController do
       expect(Writing.last.story_id).to_not be_nil
     end
   end
+
+  describe "GET show" do
+    before(:each) do
+      @story = create(:writing)
+    end
+
+    it "assigns @story to the writing with the given story_id" do
+      get :show, id: @story.story_id
+
+      expect(assigns(:story)).to eq(@story)
+    end
+  end
 end
