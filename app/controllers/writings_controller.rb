090d8f2de384af3cb2df0f1dcb172d5babb9ba0d
Nicole Paz <nicole@nic.gateway.2wire.net>
2014-03-14 16:25:09 -0700

    Add method in WritingsController to make test pass.

--- app/controllers/writings_controller.rb
+++ app/controllers/writings_controller.rb
@@ -2,4 +2,11 @@ class WritingsController < ApplicationController
   def new
     @story = Writing.new
   end
+
+  def create
+    @story = Writing.new
+    @story.name = params[:name] if params[:name] != ''
+    @story.save
+    redirect_to writing_path(@story.story_id)
+  end
 end
